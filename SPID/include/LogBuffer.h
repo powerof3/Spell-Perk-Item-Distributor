fb0e0fa26b646a22b783a578d876755658110f81
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2022-11-20 02:04:35 +0200

    Simplified implementation
    Use only message and source location to determine uniqueness of the log entry.

--- SPID/include/LogBuffer.h
+++ SPID/include/LogBuffer.h
@@ -1,84 +1,33 @@
 #pragma once
 
-#define MAKE_BUFFERED_LOG(a_func, a_type)																		\
+#define MAKE_BUFFERED_LOG(a_func)																		\
 																												\
 	template <class... Args>																					\
 	void a_func(																								\
-		RE::FormID formId,																						\
 		fmt::format_string<Args...> a_fmt,																		\
-		Args&&... a_args)																						\
+		Args&&... a_args,																						\
+		std::source_location a_loc = std::source_location::current())											\
 	{																											\
-		if (buffer.insert(Entry(a_type, formId, fmt::format(a_fmt, std::forward<Args>(a_args)...))).second)		\
+		if (buffer.insert(Entry(a_loc, fmt::format(a_fmt, std::forward<Args>(a_args)...))).second)							\
 		{																										\
-			logger::a_func(a_fmt, std::forward<Args>(a_args)...);												\
+			logger::a_func(a_fmt, std::forward<Args>(a_args)..., a_loc);												\
 		}																										\
 	}																											\
-																												\
-	template <class... Args>																					\
-	void a_func(																								\
-		std::string editorId,																					\
-		fmt::format_string<Args...> a_fmt,																		\
-		Args&&... a_args)																						\
-	{																											\
-		if (buffer.insert(Entry(a_type, editorId, fmt::format(a_fmt, std::forward<Args>(a_args)...))).second)	\
-		{																										\
-			logger::a_func(a_fmt, std::forward<Args>(a_args)...);												\
-		}																										\
-	}                                                                                               
-
-
+																												                                                                           
 
 /// LogBuffer proxies typical logging calls extended with related FormID or EditorID to differentiate
 namespace LogBuffer
 {
-	enum Type : std::int8_t
-	{
-		kTrace = 0,
-		kDebug,
-		kInfo,
-		kWarn,
-		kError,
-		kCritical
-	};
-
-	/// An object representing a single log entry, associated with a Form described with either `formId` or `editorId`.
 	struct Entry
 	{
-		Type type{ kInfo };
-		RE::FormID formId{ 0 };
-		std::string editorId{ "" };
+		std::source_location loc;
 		std::string message;
-
-		constexpr Entry(Type t, RE::FormID fid, std::string message) :
-			type(t), formId(fid), message(message) {}
-
-		constexpr Entry(Type t, std::string eid, std::string message) :
-			type(t), editorId(eid), message(message) {}
 
 		bool operator==(const LogBuffer::Entry& other) const
 		{
-			return (formId == other.formId && editorId == other.editorId && message == other.message);
+			return (strcmp(loc.file_name(), other.loc.file_name()) == 0) && loc.line() == other.loc.line() && message == other.message;
 		}
 	};
-
-	inline std::unordered_set<Entry> buffer{};
-
-	inline void clear()
-	{
-		buffer.clear();
-	};
-};
-
-/// Main log proxy functions.
-/// Each log function checks whether given message was already logged for specified Form and skips the log.
-namespace LogBuffer
-{
-	MAKE_BUFFERED_LOG(trace, kTrace);
-	MAKE_BUFFERED_LOG(debug, kDebug);
-	MAKE_BUFFERED_LOG(info, kInfo);
-	MAKE_BUFFERED_LOG(warn, kWarn);
-	MAKE_BUFFERED_LOG(error, kError);
-	MAKE_BUFFERED_LOG(critical, kCritical);
 };
 
 /// Add hashing for custom log entries.
@@ -89,21 +38,38 @@ namespace std
 	{
 		std::size_t operator()(const LogBuffer::Entry& entry) const
 		{
-			using std::hash;
-			using std::size_t;
-			using std::string;
-
-			std::size_t result = 0;
-
-			if (entry.formId > 0) {
-				result = hash<RE::FormID>()(entry.formId);
-			} else if (!entry.editorId.empty()) {
-				result = hash<std::string>()(entry.editorId);
-			}
-			// we disregard message in a hash, message will be compared when checked for equality.
-
-			// Make room for unique type of the entry.
-			return (result << 3) + entry.type;
+			return hash<std::string>()(entry.message);
 		}
 	};
 }
+
+/// Main log proxy functions.
+/// Each log function checks whether given message was already logged for specified Form and skips the log.
+namespace LogBuffer
+{
+
+	inline std::unordered_set<Entry> buffer{};
+
+	inline void clear()
+	{
+		buffer.clear();
+	};
+
+	template <class... Args>
+	void a_func(
+		fmt::format_string<Args...> a_fmt,
+		Args&&... a_args,
+		std::source_location a_loc = std::source_location::current())
+	{
+		if (buffer.insert(Entry(a_loc, fmt::format(a_fmt, std::forward<Args>(a_args)...))).second) {
+			logger::info(a_fmt, std::forward<Args>(a_args)..., a_loc);
+		}
+	}	
+
+	MAKE_BUFFERED_LOG(trace);
+	MAKE_BUFFERED_LOG(debug);
+	MAKE_BUFFERED_LOG(info);
+	MAKE_BUFFERED_LOG(warn);
+	MAKE_BUFFERED_LOG(error);
+	MAKE_BUFFERED_LOG(critical);
+};
